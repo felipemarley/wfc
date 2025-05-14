1b66c8261fc3aa2835a926f41d3a1dc41c8724ad
felipemarley <felipemarley34@gmail.com>
2025-05-13 22:27:22 -0300

    Tile script

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tile : MonoBehaviour
+{
+    public Tile[] upNeighbourgs;
+    public Tile[] rightNeighbourgs;
+    public Tile[] leftNeighbourgs;
+    public Tile[] downNeighbourgs;
+}
